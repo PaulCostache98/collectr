2f061626ea8c7014bc0015f71e1276870bd45dcf
PaulCostache98 <jales2223@gmail.com>
2023-04-03 20:47:00 +0200

    Initial setup

---
+++ src/main/java/com/gamification/collectr/CollectrApplication.java
@@ -0,0 +1,13 @@
+package com.gamification.collectr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamification/collectr/CollectrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamification.collectr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
